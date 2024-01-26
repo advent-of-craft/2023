bf9011f5a1e0d524d5b7d0665a02ed3a62a1f300
Emile RICAUD <emile.ricaud@gmail.com>
2024-01-26 23:47:11 +0100

    Finish Exercise 2

--- exercise/java/day02/src/main/java/games/FizzBuzz.java
+++ exercise/java/day02/src/main/java/games/FizzBuzz.java
@@ -1,27 +1,44 @@
 package games;
 
 public class FizzBuzz {
+
+    public static final int MIN = 0;
+    public static final int MAX = 100;
+    public static final int FIZZ = 3;
+    public static final int BUZZ = 5;
+    public static final int FIZZBUZZ = 15;
+
     private FizzBuzz() {
     }
-    
+
+    /*
+     * On extrait le range dans une methode et on fait un early throw dans le cas ou le nombre n'est pas dans le range
+     * Tous les autre input sont donc dans le range on peut donc mettre les condition en dessous
+     * Extraire les division euclidienne dans une methode
+     * A noter qu'un nombre divisible par 5 et 3 ne peut que être divisible pas 15
+     * Créer des constante
+     */
     public static String convert(Integer input) throws OutOfRangeException {
-        if (isBetween(input, 0, 100) {
-            if (input % 3 == 0 && input % 5 == 0) {
-                return "FizzBuzz";
-            }
-            if (input % 3 == 0) {
-                return "Fizz";
-            }
-            if (input % 5 == 0) {
-                return "Buzz";
-            }
-            return input.toString();
-        } else {
+        if (isNotBetween(input)) {
             throw new OutOfRangeException();
         }
+        if (canDivide(input, FIZZBUZZ)) {
+            return "FizzBuzz";
+        }
+        if (canDivide(input, FIZZ)) {
+            return "Fizz";
+        }
+        if (canDivide(input, BUZZ)) {
+            return "Buzz";
+        }
+        return input.toString();
     }
 
-    private static boolean isBetween(Integer input, Integer min, Integer max) {
-        return input >= min && input <= max;
+    private static boolean canDivide(Integer input, Integer divisor) {
+        return input % divisor == 0;
+    }
+
+    private static boolean isNotBetween(Integer input) {
+        return input <= MIN || input > MAX;
     }
 }
