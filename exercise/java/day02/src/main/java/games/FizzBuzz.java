368ba17679313174fb50aa9af39070e2cb7cdc77
Emile Ricaud <emile.ricaud@mirakl.com>
2024-01-26 17:49:45 +0100

    Ex2

--- exercise/java/day02/src/main/java/games/FizzBuzz.java
+++ exercise/java/day02/src/main/java/games/FizzBuzz.java
@@ -5,23 +5,23 @@ public class FizzBuzz {
     }
     
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
+        if (isBetween(input, 0, 100) {
+            if (input % 3 == 0 && input % 5 == 0) {
+                return "FizzBuzz";
             }
+            if (input % 3 == 0) {
+                return "Fizz";
+            }
+            if (input % 5 == 0) {
+                return "Buzz";
+            }
+            return input.toString();
         } else {
             throw new OutOfRangeException();
         }
+    }
+
+    private static boolean isBetween(Integer input, Integer min, Integer max) {
+        return input >= min && input <= max;
     }
 }
