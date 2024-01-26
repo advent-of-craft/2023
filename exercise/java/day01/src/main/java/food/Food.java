54b709d24d6cfa66706d5a19166a68be3d70dfb6
Emile Ricaud <emile.ricaud@mirakl.com>
2024-01-26 17:45:48 +0100

    Finish Exercise 1

--- exercise/java/day01/src/main/java/food/Food.java
+++ exercise/java/day01/src/main/java/food/Food.java
@@ -7,13 +7,25 @@ import java.util.function.Supplier;
 public record Food(LocalDate expirationDate,
                    Boolean approvedForConsumption,
                    UUID inspectorId) {
+
+    /*
+     * The most important is to simplify the code if/else
+     * Extract the condition in a method make more readable the code
+     * NOTE: Supplier LocalDate is used to mock the date in the test, can replace by LocalDate.now()
+     */
     public boolean isEdible(Supplier<LocalDate> now) {
-        if (this.expirationDate.isAfter(now.get()) &&
-                this.approvedForConsumption &&
-                this.inspectorId != null) {
-            return true;
-        } else {
-            return false;
-        }
+        return isFreshFood(now) && canBeConsumed() && hasBeenInspected();
+    }
+
+    private Boolean canBeConsumed() {
+        return this.approvedForConsumption;
+    }
+
+    private boolean hasBeenInspected() {
+        return this.inspectorId != null;
+    }
+
+    private boolean isFreshFood(Supplier<LocalDate> now) {
+        return this.expirationDate.isAfter(now.get());
     }
 }
